485ef378d1fd95b5e74303105630f541316dbe5c
manjunadhb <manjunadhb@gmail.com>
2017-09-09 18:13:54 +0530

    this is new project files.

---
+++ PulseFor1704/PulseFor1704/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PulseFor1704
+//
+//  Created by Manjunadh Bommisetty on 09/09/17.
+//  Copyright © 2017 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PulseFor1704/PulseFor1704/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PulseFor1704
+//
+//  Created by Manjunadh Bommisetty on 09/09/17.
+//  Copyright © 2017 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
